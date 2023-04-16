0277bb6d1e49eb7aaff0c65cb4a1f059e556cfef
marijanabevanda <marijana.bevanda4@gmail.com>
2023-04-16 13:17:23 +0200

    add unique constraint on telephone number

--- src/AddressBook.Infrastructure/Migrations/20230416101316_alter_table_ContactTelephoneNumber_add_index_TelephoneNumber_Unique.Designer.cs
+++ src/AddressBook.Infrastructure/Migrations/20230416101316_alter_table_ContactTelephoneNumber_add_index_TelephoneNumber_Unique.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using AddressBook.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace AddressBook.Infrastructure.Migrations
 {
     [DbContext(typeof(AddressBookDbContext))]
-    partial class AddressBookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230416101316_alter_table_ContactTelephoneNumber_add_index_TelephoneNumber_Unique")]
+    partial class alter_table_ContactTelephoneNumber_add_index_TelephoneNumber_Unique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AddressBook.Infrastructure/Migrations/20230416101316_alter_table_ContactTelephoneNumber_add_index_TelephoneNumber_Unique.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressBook.Infrastructure.Migrations
+{
+    public partial class alter_table_ContactTelephoneNumber_add_index_TelephoneNumber_Unique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactTelephoneNumber_TelephoneNumber",
+                table: "ContactTelephoneNumber",
+                column: "TelephoneNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContactTelephoneNumber_TelephoneNumber",
+                table: "ContactTelephoneNumber");
+        }
+    }
+}
